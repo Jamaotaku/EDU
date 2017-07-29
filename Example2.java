341ffd11fa7ba620f68965c99a857300effeb7b3
Jamaotaku <Jamaotaku@gmail.com>
2017-07-30 00:17:40 +0300

    Example.class

---
+++ Example2.java
@@ -0,0 +1,10 @@
+class Example2 {
+public static void main(String args[]) {
+int var1;
+int var2;
+var1 = 1024;
+System.out.println(" var1 = 1024" );
+var2 = var1 / 2;
+System.out.println(" var2 = " + var2);
+}
+}
---
+++ javaExample.java
@@ -0,0 +1,6 @@
+class javaExample{
+
+public static void main(String args[]){
+System.out.println ("Java drives the web ");
+}
+}
