7f22fccd5d294d8bf380b746ff9c547afe0b9980
Jamaotaku <Jamaotaku@gmail.com>
2017-07-30 21:25:25 +0300

    Added ForDemo

---
+++ GalToLit.java
@@ -0,0 +1,12 @@
+class GalToLIt{
+public static void main(String args[]){
+double liters;
+int gallons;
+
+gallons = 10 ;
+
+liters = gallons * 3.7854 ;
+
+System.out.println("В 10 Галлонах содержится "+ liters + " литров");
+}
+}
